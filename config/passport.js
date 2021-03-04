f7159fa61fa320c170f6db5540ab27d5b63f4431
scott95100 <scott95100@gmail.com>
2021-03-04 12:50:12 -0700

    passport: and set up config

--- config/passport.js
+++ config/passport.js
@@ -1,9 +1,35 @@
 require('dotenv').config();
 // A passport strategy for authenticating with a JSON Web Token
 // This allows to authenticate endpoints using a token
+const JWTStrategy = require('passport-jwt').Strategy
+const ExtractJwt= require('passport-jwt').ExtractJwt
+const mongoose = require('mongoose');
+
+//import user model 
+const { User } = require('../models/user');
 
 const options = {};
+options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+options.secretOrKey = process.env.JWT_SECRET;
 
 module.exports = (passport) => {
-    // Add code here
+    passport.use(new Strategy(options, (jwt_payload, done) => {
+        //Have a user that were find by the id inside of the payload
+        //when we get the user back, we will check to see if user in DB
+        User.findById(jwt_payload.id).then((user) => {
+            //jwt_payload is an object that contains JWT info
+            //done is a callback that we will be return user or false
+            if(user){
+                //if a user is found, return done with null(for error)
+                return done(null, user);
+            }else {
+                //no user was found
+                return done(null, false)
+            }
+        })
+        .catch(error => {
+            console.log(`====> Error below (passport.js)`)
+            console.log(error)
+        })
+    }))
 }
--- models/user.js
+++ models/user.js
@@ -1,2 +1,27 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    passport: {
+        type: String,
+        required: true,
+        minLength: 8
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+const user = mongoose.model('User', userSchema)
+
+modeles.exports = user
